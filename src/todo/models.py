6cf43fd0300f0c5792257d8b245ab0140c2e955c
homoluctus <w.slife18sy@gmail.com>
2018-07-26 19:09:05 +0900

    Add slug filed

--- src/todo/models.py
+++ src/todo/models.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User
 
 class Todo(models.Model):
     owner = models.ForeignKey(User, related_name='todos', on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-    task_name = models.CharField(max_length=100, unique=True)
+    task = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, allow_unicode=True, unique=True)
     deadline = models.DateField()
     completion = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('created_date',)
